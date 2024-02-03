b9d51b22d9ffe0155836d8490df24468bdd1f01a
ADOFO-09 <emmanueladofo011@gmail.com>
2024-02-03 23:33:20 +0000

    Challenge #2

--- fundamental 2/challenge #2.js
+++ fundamental 2/challenge #2.js
@@ -27,3 +27,4 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
 console.log(tips)
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
 console.log(bills, tips, totals)
+
