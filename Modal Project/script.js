d74625529d5bc25b35f04ef01f4e4d0f71d059ea
ADOFO-09 <emmanueladofo011@gmail.com>
2024-05-22 03:55:51 +0000

    Modal project complete

--- Modal Project/script.js
+++ Modal Project/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const modal = $(".modal");
+const overlay = $(".overlay");
+const btnCloseModal = $(".close-modal");
+const btnOpenModal = $(".show-modal");
+
+function openModal(){
+    overlay.removeClass("hidden");
+    modal.removeClass("hidden");
+}
+
+function closeModal(){
+    modal.addClass("hidden");
+    overlay.addClass("hidden");
+}
+btnOpenModal.click(function(){
+    openModal();
+});
+
+btnCloseModal.click(function(){
+   closeModal(); 
+})
+
+overlay.click(function(){
+    closeModal();
+})
+
+$("body").keydown(function(e){
+    // if(e.key === "Escape" || e.keyCode === 27){
+    //     closeModal();
+    // }
+    if(e.key === "Escape" &&  !modal.hasClass("hidden")){
+         closeModal();
+    }
+});
