c0ca61e520739b54cdcf9f6eb50d5ddcd9dc4c01
ADOFO-09 <emmanueladofo011@gmail.com>
2024-05-25 02:45:41 +0000

    Space Complexity

--- DataStructures/BigO.js
+++ DataStructures/BigO.js
@@ -1,7 +1,7 @@
-// const nemo = ['nemo'];
+const nemo = ['nemo'];
 
-// const everyone = ['Akwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-// const large = new Array(100).fill('nemo');
+const everyone = ['Akwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame','nemo'];
+const large = new Array(100).fill('nemo');
 
 // function findNemo(array){
 //     // let t0 = performance.now();
@@ -10,11 +10,29 @@
 //             console.log('Found Nemo');
 //         }
 //     }
-//     // let t1 = performance.now();
-//     // console.log("Call for findNemo is " + (t1-t0) + " milliseconds");
+    // let t1 = performance.now();
+    // console.log("Call for findNemo is " + (t1-t0) + " milliseconds");
 // }
 
-// findNemo(large);  // O(n) --> Linear Time
+// findNemo(everyone);  // O(n) --> Linear Time
+
+// Differet ways of writng loops
+// for each loop
+const findNemo2 = (array) => {
+    array.forEach(fish => {
+        if(fish === 'nemo')
+        console.log("Found Nemo")
+    })
+}
+findNemo2(nemo);
+// for of loop
+const findNemo3 = array => {
+    for (let fish of array){
+        if(fish === 'nemo')
+            console.log("Good one")
+    }
+}
+findNemo3(everyone)
 
 // const boxes = ['0','1','2','3','4','5','6','7']
 
@@ -37,20 +55,42 @@
 
 // logAllPairs(array); // O(n^2) 
 
-function printAllNumbersThenAllPairsSum(numbers){
+// function printAllNumbersThenAllPairsSum(numbers){
 
-    console.log("These are the numbers")
-    numbers.forEach(function(number){
-        console.log(number);
-    })
+//     console.log("These are the numbers")
+//     numbers.forEach(function(number){
+//         console.log(number);
+//     })
 
-    console.log("These are their sum")
-    numbers.forEach(function(firstNumber){
-        numbers.forEach(function(secondNumber){
-            console.log(firstNumber + secondNumber)
-        });
-    });
+//     console.log("These are their sum")
+//     numbers.forEach(function(firstNumber){
+//         numbers.forEach(function(secondNumber){
+//             console.log(firstNumber + secondNumber)
+//         });
+//     });
+// }
+
+// printAllNumbersThenAllPairsSum([1,2,3,4,5]);
+
+// Space complexity
+function booooo(n){
+    for(let i = 0; i < n.length ; i++){
+        console.log("booool!")
+    }
 }
 
-printAllNumbersThenAllPairsSum([1,2,3,4,5]);
+// booooo([1,2,3,4,5]); // space complexity of O(1)
+
+function arrayOfHiNTimes(n){
+    let hiArray = [];
+    for(let i = 0; i < n; i++){
+        hiArray[i] = 'hi';
+    }
+    return hiArray
+}
+
+console.log(arrayOfHiNTimes(6));   // space complexity of O(n)
+
+
+
 
