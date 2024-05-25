8d02f62a57d3fcc8faf3e37d6e52605092e11c47
ADOFO-09 <emmanueladofo011@gmail.com>
2024-05-25 01:28:18 +0000

    Quadratic Time

--- DataStructures/BigO.js
+++ DataStructures/BigO.js
@@ -16,11 +16,41 @@
 
 // findNemo(large);  // O(n) --> Linear Time
 
-const boxes = ['0','1','2','3','4','5','6','7']
+// const boxes = ['0','1','2','3','4','5','6','7']
 
-function logfirstTwoBoxes(boxes){
-    console.log(boxes[0]); // O(1) 
-    console.log(boxes[1]); // O(1) 
+// function logfirstTwoBoxes(boxes){
+//     console.log(boxes[0]); // O(1) 
+//     console.log(boxes[1]); // O(1) 
+// }
+// logfirstTwoBoxes(boxes); // O(2)
+
+// O(n^2)
+// const array = ['a','b','c','d','e','f']
+
+// function logAllPairs(){
+//     for(let i = 0; i < array.length; i++){
+//         for(let j = 0; j < array.length; j++){
+//             console.log(array[i] + " " + array[j])
+//         }
+//     }
+// }
+
+// logAllPairs(array); // O(n^2) 
+
+function printAllNumbersThenAllPairsSum(numbers){
+
+    console.log("These are the numbers")
+    numbers.forEach(function(number){
+        console.log(number);
+    })
+
+    console.log("These are their sum")
+    numbers.forEach(function(firstNumber){
+        numbers.forEach(function(secondNumber){
+            console.log(firstNumber + secondNumber)
+        });
+    });
 }
-logfirstTwoBoxes(boxes); // O(2)
+
+printAllNumbersThenAllPairsSum([1,2,3,4,5]);
 
