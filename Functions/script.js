fe096118f273063b1009c9512e92f1afb41f9f74
ADOFO-09 <emmanueladofo011@gmail.com>
2024-06-26 11:05:42 +0000

    value and reference

--- Functions/script.js
+++ Functions/script.js
@@ -2,23 +2,51 @@
 
 //Default Parameters
 
-const bookings = [];
+// const bookings = [];
 
-const createBookings = function(
-    flightNum, 
-    numPassengers = 2,
-    price = 199 * numPassengers
-    )
-{
-    const booking = {
-        flightNum,
-        numPassengers,
-        price
+// const createBookings = function(
+//     flightNum, 
+//     numPassengers = 2,
+//     price = 199 * numPassengers
+//     )
+// {
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price
+//     }
+
+//     console.log(booking)
+//     bookings.push(booking)
+// }
+
+// createBookings('LH123', undefined, undefined)
+// createBookings('LH123', 3);
+
+const flight = 'LH123';
+const Emma = {
+    name: 'Emmanuel Adofo',
+    passport: 247520600,
+};
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 247520600){
+        alert('Checked In');
+    } else {
+        alert('Wrong passport');
     }
+};
 
-    console.log(booking)
-    bookings.push(booking)
-}
+checkIn(flight, Emma);
+console.log(flight);
+console.log(Emma);
 
-createBookings('LH123', undefined, undefined)
-createBookings('LH123', 3);
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+
+newPassport(Emma);
+checkIn(flight, Emma);
