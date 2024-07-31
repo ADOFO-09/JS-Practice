66ca130b52872389acaf7f7320dab55c38009c03
ADOFO-09 <emmanueladofo011@gmail.com>
2024-07-31 04:07:06 +0000

    Bankist app and challenge

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+       <div class="movements__row">
+          <div class="movements__type 
+          movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+} 
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -147,13 +165,15 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-
+// MAP
 currencies.forEach(function(value,key,map){
   console.log(`${key}: ${value}`);
 });
 
+// SET
 const currenciesUnique = new Set(['USD', 'GBP', 'USD' , 'EUR', 'EUR']);
 console.log(currenciesUnique);
 currenciesUnique.forEach(function(value, _ , map){
   console.log(`${value}: ${value}`);
 });
+
