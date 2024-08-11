ba00073733b94d8ab446a8e837d810816b627c5d
ADOFO-09 <emmanueladofo011@gmail.com>
2024-08-11 00:07:09 +0000

    Working with array methods map,filter & reduce

--- 11-Arrays-Bankist/starter/challenge1.js
+++ 11-Arrays-Bankist/starter/challenge1.js
@@ -29,12 +29,62 @@ const checkDogs = function(dogsJulia, dogsKate) {
     const dogs = CorrectedJulia.concat(dogsKate)
     console.log(dogs);
     dogs.forEach(function(dog, i){
-        const age = dog >= 3 ? `Dog number ${i + 1} is an adult, and is ${dog} years old`:
-         `Dog number ${i + 1} is still a puppy 🐶`;
+        const age = dog >= 3 ? 
+        `Dog number ${i + 1} is an adult, and is ${dog} years old`:
+        `Dog number ${i + 1} is still a puppy 🐶`;
         console.log(age);
     })
 }
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3] )
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4] )
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4] )
+
+
+// Coding Challenge #2 
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
+// dog ages to human ages and calculate the average age of the dogs in their study. 
+// Your tasks: 
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+// ages ('ages'), and does the following things in order: 
+// 1. Calculate the dog age in human years using the following formula: if the dog is 
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+// humanAge = 16 + dogAge * 4 
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as 
+// keeping dogs that are at least 18 years old) 
+// 3. Calculate the average human age of all adult dogs (you should already know 
+// from other challenges how we calculate averages 
+// �
+// �) 
+// 4. Run the function for both test datasets 
+// Test data: 
+// § Data 1: [5, 2, 4, 1, 15, 8, 3] 
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function(ages){
+    // const humanAge = ages.map(function(age){
+    //     if(age <= 2){
+    //        return age * 2
+    //     }else if (age > 2){
+    //         return 16 + age * 4
+    //     }
+    // })
+    const humanAge = ages.map(age => age <= 2 ? age * 2 : 16 + age * 4)
+    // Exclude all dogs that are less than 18 human years old
+    const Adults = humanAge.filter(age => age > 18)
+    // Average human age of all adult dogs
+    const average = Adults.reduce((acc, age) => acc + age, 0) / Adults.length
+    return average 
+}
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2)
+
+
+
+
+
+
+
+
+
 
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -79,6 +79,27 @@ const displayMovements = function(movements){
   })
 } 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function(movements){
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${balance} EUR`
+}
+calcDisplayBalance(account1.movements)
+
+
+const createUsernames = function(accs){
+  accs.forEach((acc) =>
+    acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('')
+  )
+}
+createUsernames(accounts);
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -160,20 +181,89 @@ movements.forEach(function(mov, i){
 })
 */
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-// MAP
-currencies.forEach(function(value,key,map){
-  console.log(`${key}: ${value}`);
-});
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+// // MAP
+// currencies.forEach(function(value,key,map){
+//   console.log(`${key}: ${value}`);
+// });
 
-// SET
-const currenciesUnique = new Set(['USD', 'GBP', 'USD' , 'EUR', 'EUR']);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function(value, _ , map){
-  console.log(`${value}: ${value}`);
-});
+// // SET
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD' , 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function(value, _ , map){
+//   console.log(`${value}: ${value}`);
+// });
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function(mov){
+//   return mov * eurToUsd;
+// });
+// const movementsUSD = movements.map(mov => mov * eurToUsd)
+// console.log(movements);
+// console.log(movementsUSD);
+
+
+// const movementsUSDfor = [];
+// for(const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+
+// const movementsDescription = movements.map((mov, i) => 
+//   `Movement ${i + 1}: You  ${mov > 0? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+// );
+
+// console.log(movementsDescription);
+
+// filter method functions with respect to a condition
+ const deposit = movements.filter(function(mov){
+   return mov > 0;
+ })
+
+console.log(movements);
+console.log(deposit);
+
+const depositfor = [];
+for(const mov of movements) if(mov > 0) depositfor.push(mov);
+console.log(depositfor);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
+
+const withdrawalfor = [];
+for(const mov of movements) if(mov < 0) withdrawalfor.push(mov);
+console.log(withdrawalfor);
+
+// Reduce Method
+// const balance = movements.reduce(function(acc, cur,i, arr){
+//    console.log(`Iteration ${i}: ${acc}`);
+//    return acc + cur
+// }, 0)
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for(const mov of movements) balance2 += mov;
+console.log(balance2);
+
+//Maximum value 
+const max = movements.reduce((acc, mov) => {
+    if(acc > mov) return acc;
+    else return mov; 
+}, movements[0]);
+console.log(max);
+
+const min = movements.reduce((acc, mov) => {
+  if(acc > mov) return mov;
+  else return acc;
+}, movements[0]);
+console.log(min);
+
+
+
+
 
